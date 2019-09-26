7b4b5ede29a14edf4084619f39d5fa172d764c56
sjw-4 <55467048+sjw-4@users.noreply.github.com>
2019-09-26 13:43:28 -0500

    Add files via upload

--- free_list.cpp
+++ free_list.cpp
@@ -59,9 +59,15 @@ SegmentHeader::SegmentHeader(size_t _length, bool _is_free) {
   is_free = _is_free;
   cookie = COOKIE_VALUE;
   next = nullptr; prev = nullptr;
+  lr = 'l';
+  inherit = 'l';
 }
 
+
 SegmentHeader::~SegmentHeader() {
+	next = nullptr; prev = nullptr;
+	length = -1;
+	lr = 'z'; inherit = 'z';
 }
 
 
@@ -74,7 +80,17 @@ bool SegmentHeader::CheckValid() {
 /* FUNCTIONS FOR CLASS FreeList */
 /*--------------------------------------------------------------------------*/
 
+FreeList::FreeList() {
+	head = nullptr;
+}
+
+FreeList::FreeList(SegmentHeader* _segment) {
+	head = nullptr;
+	Add(_segment);
+}
+
 FreeList::FreeList(void* addr, size_t _size) {
+	head = nullptr;
 	Add(new (addr) SegmentHeader(_size));
 }
 
@@ -82,6 +98,7 @@ FreeList::~FreeList() {
 }
 
 bool FreeList::Add(SegmentHeader * _segment) {
+	//cout << "Adding size " << _segment->length << endl;
 	if (head == nullptr) {
 		head = _segment;
 	}
@@ -90,23 +107,26 @@ bool FreeList::Add(SegmentHeader * _segment) {
 		_segment->next = head;
 		head = _segment;
 	}
+	_segment->is_free = true;
 	return true;
 }
 
 bool FreeList::Remove(SegmentHeader * _segment) {
-	if (_segment == head && head->next == nullptr) {
+	if (_segment == head && head->next == nullptr) {	//segment is only SH in FL
 		head = nullptr;
 	}
-	else if (_segment == head) {
+	else if (_segment == head) {		//segment is head in FL, makes next item in FL head
 		_segment->next->prev = nullptr;
 		head = head->next;
 	}
-	else if (_segment->next == nullptr) {
+	else if (_segment->next == nullptr) {	//segment is tail in FL, makes prev item the new tail
 		_segment->prev->next = nullptr;
 	}
-	else {
+	else {		//segment is somewhere in the middle, removes it and sets proper next and prev pointers
 		_segment->next->prev = _segment->prev;
 		_segment->prev->next = _segment->next;
 	}
+	_segment->is_free = false;
+	_segment->prev = nullptr; _segment->next = nullptr;
 	return true;
 }
